4e61e5e4002ce3da033f77ccc54d5b2a5dc7aaae
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-29 17:19:10 -0400

    updated till adding dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,26 +3,66 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
- const createTweetElement = function (tweet) {
-    const $tweet = $(`<article class="tweet">Hello world</article>`)
+const createTweetElement = function (tweet) {
+    const { user, content, created_at } = tweet;
+    const $tweet = $(` <article class="tweet-box">
+    <header class="tweet-header">
+      <div class="tweet-author">
+        <img src=${user.avatars}  class="tweet-avatar">
+        <h3 class="tweet-author-name">${user.name}</h3>
+      </div>
+      <p class="tweet-author-username">${user.handle}</p>
+    </header>
+    <div class="tweet-content">
+      <p class="tweet-text1"> ${content.text}</p>
+    </div>
+    <div class="new-tweetline1"></div>
+    <output name="counter1" class="counter1" for="tweet-text1">${created_at}</output>
+    <footer class="tweet-footer">
+      <i class="far fa-flag tweet-icon"></i>
+      <i class="fas fa-retweet tweet-icon"></i>
+      <i class="far fa-heart tweet-icon"></i>
+    </footer>
+  </article>`)
     return $tweet
 }
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
+const tweetData = [{
     "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
         "handle": "@SirIsaac"
-      },
+    },
     "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+    },
     "created_at": 1461116232227
- }
+},
+{
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1680033989069
+  }]
+$(document).ready(function () {
+    const renderTweets = function (tweets) {
+        console.log(tweets);
+        for (tweet of tweets) {
+            const $tweet = createTweetElement(tweet);
 
-const $tweet = createTweetElement(tweetData);
+            // Test / driver code (temporary)
+            console.log($tweet); // to see what it looks like
+            $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+        }
+        return;
+    }
+    renderTweets(tweetData);
+        })
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
 
