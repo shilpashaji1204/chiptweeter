e53d46434d1d5b7e927f1aed0fbd0dc67fc00c86
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-29 16:06:11 -0400

    updated till jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,26 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+ const createTweetElement = function (tweet) {
+    const $tweet = $(`<article class="tweet">Hello world</article>`)
+    return $tweet
+}
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+ }
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
